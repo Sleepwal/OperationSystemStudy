16b86ede36e6e17355f60f54b6f02a90658fa015
SleepWalker <SleepWalker@qq.com>
2023-06-01 10:00:50 +0800

    move

---
+++ lesson/less2/fgFun_os.h
@@ -0,0 +1,34 @@
+#include<assert.h>
+#include <cstdio>
+
+typedef struct {
+  int pc, n;
+  char type;
+} Frame;
+
+#define call(...) ({ *(++top) = (Frame) { .pc = 0, __VA_ARGS__ }; })
+#define ret()     ({ top--; })
+#define goto(loc) ({ f->pc = (loc) - 1; })
+
+int fg_os(int n, char type) {
+  int sum = 0;
+  Frame stk[64], *top = stk - 1;
+  call(n, type);
+  for(Frame *f; (f = top) >= stk; f->pc++) {
+    n = f->n; type = f->type;
+    switch (f->pc) {
+      case 0: if(n <= 1) { sum++; goto(2); } break;
+      case 1:
+        call(n - 1, type);
+        if(type == 'f') 
+          call(n - 2, 'g');
+        else 
+          call(n + 1, 'f');
+        break;
+      case 2: ret(); break;
+      default: assert(0);
+    }
+  }
+
+  return sum;
+}
